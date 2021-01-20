be307ac135c849952bae2e3d4fff25615d7428ab
gage3121 <gagenhall@gmail.com>
2021-01-19 19:43:27 -0600

    practiced arrays

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Arrays")
+const student = ['Gage', 'Dani', 'Gabrielle', 'Nathan'];
+//console.log(student.length)
+
+//***** ACCESSING ITEMS IN ARRAY
+//console.log(student[1]);
+//console.log(student[student.length - 1]); //Nathan
+
+student[0] = 'Matthew';
+//console.log(student);
+
+const valuePrinter = (array, index) => {
+    return array[index];
+}
+
+//console.log(valuePrinter(student, 2));
+
+//Write a function that takes an array of something and tells you if the name 'Greg' is in that array
